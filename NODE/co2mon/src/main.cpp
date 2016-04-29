c066ff4bc0e17370b86c1d05f2560d5acdb8abcb
acaaad <auliarshad@gmail.com>
2016-04-29 23:25:10 +0700

    tambah rtc

--- NODE/co2mon/src/main.cpp
+++ NODE/co2mon/src/main.cpp
@@ -1,4 +1,3 @@
-
 /* Include FreeRTOS */
 #include "FreeRTOS_AVR.h"
 #include <Arduino.h>
@@ -6,15 +5,22 @@
 #include <SoftwareSerial.h>
 #include <cozir.h>
 #include <stdio.h>
+#include <DS3231.h>
 //#include <time.h>
 
 SoftwareSerial sws(13,12);
 COZIR czr(&sws);
 
-
-
+// Init the DS3231 using the hardware interface
+DS3231  rtc(SDA, SCL);
+Time w;
 #define t 1
 #define f 0
+#define ANALOG_SENSOR_PIN A0
+#define DIGITAL_SENSOR_PIN 4
+
+static int stateDigital;
+static int lightAnalogValue;
 
 typedef struct {
   float usVal;
@@ -30,8 +36,9 @@ static volatile resultF resTemp = {0.0, f};
 static volatile resultCO2 resCO2 = {0, f};
 static volatile resultF resHum = {0.0, f};
 //static volatile result resTemp = {0, f};
-
-TaskHandle_t temp, co2, hum, light, print;
+static char *old_time = "";
+static char *taimu = "";
+TaskHandle_t temp, co2, hum, light, print, clock;
 
 static void vTempReader ( void *pvParameter){
   //Serial.print(F("Unused Stack Temp: "));
@@ -84,10 +91,16 @@ static void vHumidityReader ( void *pvParameter){
   }
 }
 
-/*
-static void vLightReader ( void *pvParameter){
 
-}*/
+static void vLightReader ( void *pvParameter){
+  for( ;; ){
+    vTaskSuspendAll();
+    {
+      lightAnalogValue = analogRead(ANALOG_SENSOR_PIN);
+    }
+    xTaskResumeAll();
+  }
+}
 
 static void vPrintTask ( void *pvParameter ){
   //Serial.print(F("Unused Stack Print: "));
@@ -98,26 +111,29 @@ static void vPrintTask ( void *pvParameter ){
       vTaskSuspendAll();
     	{
         if(resCO2.ucStatus==t && resTemp.ucStatus==t && resHum.ucStatus==t){
-          /*
-          Serial.print(F("Temp: "));
-          Serial.print(resTemp.usVal);
-          Serial.print(F(" "));
-          Serial.print(F("CO2: "));
-          Serial.print(resCO2.ulCO2Val);
-          Serial.print(F(" "));
-          Serial.print("Hum: ");
-          Serial.println(resHum.usVal);
-          */
 
-          Serial.print(resCO2.ulCO2Val);
-          Serial.print(F(","));
-          Serial.print(resTemp.usVal);
-          Serial.print(F(","));
-          Serial.println(resHum.usVal);
+            w =rtc.getTime();
+            Serial.print(resCO2.ulCO2Val);
+            Serial.print(F(","));
+            Serial.print(resTemp.usVal);
+            Serial.print(F(","));
+            Serial.print(resHum.usVal);
+            Serial.print(F(","));
+            Serial.print(lightAnalogValue);
+            Serial.print(F(","));
+            Serial.print(rtc.getDateStr());
+            Serial.print(F(","));
+            Serial.print(rtc.getTimeStr());
+            Serial.print(F(","));
+            Serial.println(w.sec);
 
+          //Serial.println(old_time);
+          //Serial.println(taimu);
+          //Serial.println("Keluar");
           resTemp.ucStatus = f;
           resCO2.ucStatus = f;
           resHum.ucStatus = f;
+
         }
         else{
         }
@@ -126,17 +142,34 @@ static void vPrintTask ( void *pvParameter ){
     }
 
 }
+/*
+static void vClockRTC ( void *pvParameter ){
+  for( ;; ){
+      vTaskSuspendAll();
+    	{
+        if(resCO2.ucStatus==t && resTemp.ucStatus==t && resHum.ucStatus==t){
+
+        }
+        else{
+        }
+    	}
+    	xTaskResumeAll();
+    }
+
+}*/
 
 void setup( void ){
   sws.begin(9600);
   Serial.begin(9600);
+  rtc.begin();
   //czr.init();
 
   xTaskCreate( vTempReader, "TempReader", configMINIMAL_STACK_SIZE + 250, NULL, 1, &temp);
   xTaskCreate( vCO2Reader, "CO2Reader", 500, NULL, 1, &co2);
   xTaskCreate( vHumidityReader, "HumidityReader", configMINIMAL_STACK_SIZE + 100, NULL, 1, &hum);
-  //xTaskCreate( vLightReader, "LightReader", 200, NULL, 2, &light);
+  xTaskCreate( vLightReader, "LightReader", 400, NULL, 1, &light);
   xTaskCreate( vPrintTask, "PrintTask", 400, NULL, 1, &print);
+  //xTaskCreate( vClockRTC, "PrintTask", 400, NULL, 1, &clock);
 
   /*
   * UNCOMMENT TO VIEW AMOUNT OF FREE HEAP LEFT
